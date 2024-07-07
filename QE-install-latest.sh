ee0bad30b213f18fb4e92ad807623c4c7a354693
farhanwaris78 <55206873+farhanwaris78@users.noreply.github.com>
2024-07-07 20:47:42 +0500

    Update QE-install-latest.sh
    0.0.2

--- QE-install-latest.sh
+++ QE-install-latest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 # Update list of repositories and upgrade the system
 sudo apt -y update
 sudo apt -y upgrade
@@ -8,29 +10,43 @@ sudo apt -y upgrade
 # Git, wget, gcc/g++/gfortran, LAPACK, FFTW, openmpi (mpirun)
 sudo apt -y install git wget build-essential g++ gfortran liblapack-dev libfftw3-dev libopenmpi-dev
 
-#Download latest git file of QE
+# Download latest git file of QE
 git clone https://github.com/QEF/q-e.git
 cd q-e
 
+# Configure and build QE
 ./configure
-
 make all
 make w90
 
+# Add QE binaries to PATH
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo 'export PATH="'"$SCRIPT_DIR"'/q-e/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+echo 'export PATH="'"$SCRIPT_DIR"'/bin:$PATH"' >> ~/.bashrc
 
-# Quantum ESPRESSO and Wannier90
-sudo apt -y install quantum-espresso wannier90
+# Go back to the original directory
+cd ..
 
 # Additional tools
 sudo apt -y install xcrysden gnuplot
 sudo apt -y install python3-dev python3-pip
 pip3 install numpy scipy sympy matplotlib jupyterlab
-#echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-#source ~/.bashrc
+
+# Source .bashrc to apply changes immediately
+source ~/.bashrc
 
 # Completion message
+echo "
+   ______     __  __     ______     __   __     ______   __  __     __    __            
+  /\  __ \   /\ \/\ \   /\  __ \   /\ \-.\ \   /\__  _\ /\ \/\ \   /\ \-./  \           
+  \ \ \/\_\  \ \ \_\ \  \ \  __ \  \ \ \-.  \  \/_/\ \/ \ \ \_\ \  \ \ \-./\ \          
+   \ \___\_\  \ \_____\  \ \_\ \_\  \ \_\ \\_\    \ \_\  \ \_____\  \ \_\ \ \_\         
+    \/___/_/   \/_____/   \/_/\/_/   \/_/ \/_/     \/_/   \/_____/   \/_/  \/_/         
+                                                                                        
+   ______     ______     ______   ______     ______     ______     ______     ______    
+  /\  ___\   /\  ___\   /\  == \ /\  == \   /\  ___\   /\  ___\   /\  ___\   /\  __ \   
+  \ \  __\   \ \___  \  \ \  _-/ \ \  __<   \ \  __\   \ \___  \  \ \___  \  \ \ \/\ \  
+   \ \_____\  \/\_____\  \ \_\    \ \_\ \_\  \ \_____\  \/\_____\  \/\_____\  \ \_____\ 
+    \/_____/   \/_____/   \/_/     \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_____/ 
+                                                                                     "
 echo "Installation finished!"
-echo "Let's start our journey with QuantumESPRESSO!"
+echo "Let's start our journey with the latest version of QuantumESPRESSO!"
