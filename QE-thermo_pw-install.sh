f15ecaa3f4d9009f28f1987ad9a3a1a7fa9bdf19
farhanwaris78 <55206873+farhanwaris78@users.noreply.github.com>
2024-10-25 12:37:07 +0500

    Create QE-thermo_pw-install.sh

---
+++ QE-thermo_pw-install.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+                                                            # use cmd "bash QE-install-latest.sh" to install it
+
+set -e  # Exit immediately if a command exits with a non-zero status.
+set -u  # Treat unset variables as an error when substituting.
+
+# Function to print colored output
+print_colored() {
+    echo -e "\e[1;34m$1\e[0m"
+}
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Update list of repositories and upgrade the system
+print_colored "Updating and upgrading system packages..."
+sudo apt update && sudo apt upgrade -y
+
+# Install important development tools and libraries
+print_colored "Installing development tools and libraries..."
+sudo apt install -y git wget build-essential g++ gfortran liblapack-dev libfftw3-dev libopenmpi-dev libblas-dev
+
+# Download latest git file of QE
+print_colored "Cloning Quantum ESPRESSO repository..."
+wget https://github.com/QEF/q-e/archive/refs/tags/qe-7.3.tar.gz            # qe version 7.3 (2023) from repository, as tar.gz file
+tar -xvzf qe-7.3.tar.gz
+cd q-e-qe-7.3
+
+# downloading thermo_pw tar file into QE directory
+wget http://people.sissa.it/%7Edalcorso/thermo_pw/thermo_pw.2.0.0.tar.gz
+
+
+# Configure and build QE
+print_colored "Configuring Quantum ESPRESSO..."
+./configure
+
+print_colored "Building Quantum ESPRESSO..."
+make -j$(nproc) all                                           # -j$(nproc) is used to use all cpus to install efficiently the program
+make -j$(nproc) w90                                           # other wise remove the aboe part
+
+# Add QE binaries to PATH in .bashrc file
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo 'export PATH="'"$SCRIPT_DIR"'/bin:$PATH"' >> ~/.bashrc
+
+# Go back to the original directory
+cd ..
+
+
+# For Burai 1.3.2 gui pkg for QE
+# IF YOU WANT TO TUSE BURAI GUI FOR QE THEN REMOVE # FROM 1ST BELOW LINE TO 9TH LINE BELLOW OTHE WISE PLACE # IN START OF LINE
+print_colored "Installing Burai 1.3.2  ..."
+sudo apt-get install openjdk-8-jdk -y                       # open java library sized at ~650mb
+sudo apt-get install openjfx -y                             # Open jfx library sized at ~220mb
+wget https://raw.githubusercontent.com/mampaiva/fix-burai/master/fix-burai.sh 						# to fix burai not opening issue run this cmd then use "bash ./fix-burai.sh"
+wget https://github.com/BURAI-team/burai/releases/download/ver.1.3.2/BURAI1.3.2_Linux.tgz
+tar zxvf BURAI1.3.2_Linux.tgz
+rm BURAI1.3.2_Linux.tgz
+cd BURAI1.3.2
+bash makeLauncher.sh
+cd ..
+# if burai does not show crystal structure type "java -Dprism.forceGPU=true -jar ./bin/burai.jar" in burai.sh located in BURAI1.3.2/bin
+
+
+# Install PWgui
+# IF YOU DO NOT WANT TO INSTALL PWGUI THEN PLACE # IN START OF LINE FROM 1ST BELLOW TO 9TH BELLOW LINE
+print_colored "Installing PWgui..."
+PWGUI_VERSION="7.0"
+PWGUI_FILE="pwgui-${PWGUI_VERSION}-linux-x86_64.tgz"
+wget "http://www-k3.ijs.si/kokalj/pwgui/download/${PWGUI_FILE}"
+tar zxvf "$PWGUI_FILE"
+rm "$PWGUI_FILE"
+
+# Add PWgui to PATH in .bash file
+echo 'export PATH="'"$PWD"':$PATH"' >> ~/.bashrc
+
+# Create symlink for pwgui
+sudo ln -sf "$PWD/pwgui" /usr/local/bin/pwgui
+
+# Install additional tools
+print_colored "Installing additional tools..."
+sudo apt install -y xcrysden bc
+sudo apt install gnuplot python3-dev python3-pip
+pip3 install --upgrade pip
+pip3 install numpy scipy sympy matplotlib jupyterlab
+pip3 install cif2cell
+
+# For thermoelectric properties BoltzTraP2 is needed for quantum-ESPRESSO
+print_colored "Installing BoltzTraP2..."
+pip3 install cython spglib netCDF4 ase cmake pyfftw vtk
+pip3 install BoltzTraP2
+# git clone --depth 1 https://gitlab.com/sousaw/BoltzTraP2.git
+# cd BoltzTraP2
+# python3 setup.py install
+# cd ..
+# to check if BoltzTrap is install use cmd "btp2 -h"
+
+
+# Source .bashrc to apply changes immediately
+source ~/.bashrc
+
+# Completion message
+print_colored "
+   ______     __  __     ______     _    __     ______   __  __     __    __            
+  /\  __ \   /\ \/\ \   /\  __ \   /\  -.\ \   /\__  _\ /\ \/\ \   /\  -./  \           
+  \ \ \/\_\  \ \ \_\ \  \ \  __ \  \ \ \ _  \  \/_/\ \/ \ \ \_\ \  \ \ \-./\ \          
+   \ \___\_\  \ \_____\  \ \_\ \_\  \ \_\  \_\    \ \_\  \ \_____\  \ \_\ \ \_\         
+    \/___/_/   \/_____/   \/_/\/_/   \/_/ \/_/     \/_/   \/_____/   \/_/  \/_/         
+                                                                                        
+   ______     ______     ______   ______     ______     ______     ______     ______    
+  /\  ___\   /\  ___\   /\  == \ /\  == \   /\  ___\   /\  ___\   /\  ___\   /\  __ \   
+  \ \  __\   \ \___  \  \ \  _-/ \ \  __<   \ \  __\   \ \___  \  \ \___  \  \ \ \/\ \  
+   \ \_____\  \/\_____\  \ \_\    \ \_\ \_\  \ \_____\  \/\_____\  \/\_____\  \ \_____\ 
+    \/_____/   \/_____/   \/_/     \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_____/ 
+                                                                                     "
+print_colored "Installation finished!"
+print_colored "Let's start our journey with the latest version of QuantumESPRESSO!"
+# IF PW.X DOES NOT WORK,THEN RESTART THE SYSTEM
